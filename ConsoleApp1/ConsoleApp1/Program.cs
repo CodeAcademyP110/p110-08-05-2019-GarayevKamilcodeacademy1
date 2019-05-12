ddfaab2b7490e73a689a8f94c9699473a859e84b
GarayevKamilcodeacademy1 <46805946+GarayevKamilcodeacademy1@users.noreply.github.com>
2019-05-12 16:48:39 +0400

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////task1
+            //Console.WriteLine("eded daxil edin:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int total = 0;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    total = total + i;
+            //}
+            //Console.WriteLine("cem:");
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+
+            ////task 2
+            //Console.WriteLine("eded daxil edin:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int total = 0;
+            //for (int i = 1; i <= a; i++)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        total = total + i;
+            //    }
+
+            //}
+            //Console.WriteLine("cem:");
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            ////task3
+            //int number = int.Parse(Console.ReadLine());
+            //int total = 0;
+            //while (number > 0)
+            //{
+
+            //    total += number % 10;
+            //    Console.WriteLine(number % 10);
+            //    number /= 10;
+
+            //}
+            //Console.WriteLine("cem");
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+
+            ////task4
+            //Console.WriteLine("enter email:");
+            //string a = Console.ReadLine();
+            //if (a.IndexOf("@")==-1)
+            //{
+            //    Console.WriteLine("Error Error Error ErrorErrorError ErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorError Error Error Error vv vErrorErrorErrorErrorErrorErrorErrorErrorErrorErrorError");
+            //    Console.Read();
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Düzgün email daxil etmisiniz, təbriklər, kələ kimisiz");
+            //    Console.Read();
+            //}
+
+        }
+
+
+
+    }
+}
